878adff18dc7ef42bd3f35b334433a43c9b3fac9
Nova-Bontes <nova.motsoari@gmail.com>
2025-09-24 17:29:24 +0200

    description on how to run server

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # online-book-review
+
+1. Prerequisites
+
+- Node.js (v14+)
+- npm
+
+2. Install dependencies:
+   npm init -y
+   npm install express body-parser express-session jsonwebtoken bcrypt axios
+
+3. Run the server:
+   node server.js
--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const reviewRoutes = require("./routes/reviews");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(express.json());
 
 app.use("/books", bookRoutes);
 app.use("/users", userRoutes);
 app.use("/reviews", reviewRoutes);
 
 app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+
